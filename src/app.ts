ff070fccaec20dcde29ae93cd76ef75aa1fa1ae0
Wang HZ <36946658+wang-hz@users.noreply.github.com>
2026-05-29 23:25:31 +0800

    fix: enable trust proxy to resolve express-rate-limit X-Forwarded-For error

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ import path from 'path';
 
 const app = express();
 
+app.set('trust proxy', 1);
 app.use(cors({ origin: CORS_ORIGIN, credentials: true }));
 app.use(helmet({
   contentSecurityPolicy: {
